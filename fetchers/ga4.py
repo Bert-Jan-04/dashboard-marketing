2b3a204f8c9a6b5f4f1f1feafa139c699824dd1c
Bert-Jan <russchenbertjan@gmail.com>
2026-05-22 13:35:44 +0200

    Fix: alle fetchers gebruiken dezelfde kalenderweek (ma t/m gisteren)

--- fetchers/ga4.py
+++ fetchers/ga4.py
@@ -22,7 +22,7 @@ DATA_FILE = os.path.join(ROOT_DIR, "data", "ga4_data.json")
 
 today      = date.today()
 week_end   = today - timedelta(days=1)
-week_start = week_end - timedelta(days=6)
+week_start = week_end - timedelta(days=week_end.weekday())
 prev_end   = week_start - timedelta(days=1)
 prev_start = prev_end - timedelta(days=6)
 
--- fetchers/gsc.py
+++ fetchers/gsc.py
@@ -16,8 +16,8 @@ DATA_FILE = os.path.join(ROOT_DIR, "data", "gsc_data.json")
 SCOPES    = ["https://www.googleapis.com/auth/webmasters.readonly"]
 
 today      = date.today()
-week_end   = today - timedelta(days=3)
-week_start = week_end - timedelta(days=6)
+week_end   = today - timedelta(days=1)
+week_start = week_end - timedelta(days=week_end.weekday())
 prev_end   = week_start - timedelta(days=1)
 prev_start = prev_end - timedelta(days=6)
 
